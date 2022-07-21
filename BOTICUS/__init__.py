22fe9218a9e53d0b70e77a6725157ae2519c42d5
99dreada <99dreada@live.co.uk>
2022-07-21 10:47:19 +0100

    db init

--- BOTICUS/__init__.py
+++ BOTICUS/__init__.py
@@ -1,8 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
+db_name = 'boticus.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
 from BOTICUS import routes
+
+@app.route('/dbtest')
+def testdb():
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>IT WORKS!!!</h1>'
+    except Exception as e:
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken</h1>'
+        return hed + error_text
 
 if __name__ == '__main__':
    app.run()
