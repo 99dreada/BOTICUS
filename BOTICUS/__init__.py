f38d74bafffa7d6f83f1bd6bec73d24804dfeda4
99dreada <99dreada@live.co.uk>
2022-07-21 15:28:54 +0100

    updates

--- BOTICUS/__init__.py
+++ BOTICUS/__init__.py
@@ -1,27 +1,27 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+from flask.cli import AppGroup
+import click
+import os
+from BOTICUS.model import db
 
 app = Flask(__name__)
 
-db_name = 'boticus.db'
+def config_db(db_url='sqlite:///db/boticus.db'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = "#########"
+    db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
-
+config_db()
 from BOTICUS import routes
 
-@app.route('/dbtest')
-def testdb():
-    try:
-        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-        return '<h1>IT WORKS!!!</h1>'
-    except Exception as e:
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken</h1>'
-        return hed + error_text
+db_cli = AppGroup('db')
 
-if __name__ == '__main__':
-   app.run()
+@db_cli.command('init')
+def db_init():
+    try: os.mkdir('boticus/db')
+    except: pass
+    from BOTICUS.model import create_db
+    create_db()
+
+app.cli.add_command(db_cli)
---
+++ BOTICUS/model.py
@@ -0,0 +1,48 @@
+from enum import unique
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+
+db = SQLAlchemy()
+
+class User_sql(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, index=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class Bots_sql(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    api_key = db.Column(db.String(100), unique=True)
+    status = db.Column(db.Integer)
+
+class Channels_sql(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    channelid = db.Column(db.Integer)
+
+
+"""
+INIT DATABASE
+"""
+### uncomment out below for table with prepopulated data
+
+#INIT_TABLES = []
+INIT_DIRECTORY_NAME = "BOTICUS/db/initial"
+TABLES_TO_SAVE = [
+    Bots_sql,
+    Channels_sql
+]
+
+def create_db():
+    try: db.drop_all()
+    except: pass
+    db.create_all()
+    #import csv
+    #import os
+    #for table in INIT_TABLES:
+        #filename = table.__name__ + '.csv'
+        #filename = os.path.join(INIT_DIRECTORY_NAME, filename)
+        #with open(filename, newline='') as file:
+            #for row in csv.DictReader(file):
+                #db.session.add(table(**row))
+    db.session.commit()
