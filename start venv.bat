6eab7d900e517c2c072e73cfcb39473945d5f138
99dreada <99dreada@live.co.uk>
2022-07-25 12:07:41 +0100

    updated bat file

--- start venv.bat
+++ start venv.bat
@@ -1,1 +1,45 @@
+@ECHO OFF
+CLS
 call "venv/Scripts/activate.bat"
+
+:begin
+CLS
+ECHO 1.Full Setup (Run this if first time setup)
+ECHO 2.Install requirements
+ECHO 3.Build DB
+ECHO 4.Run Application
+ECHO.
+CHOICE /C 1234 /M "Enter your choice:"
+
+IF ERRORLEVEL 4 GOTO Run
+IF ERRORLEVEL 3 GOTO Builddb
+IF ERRORLEVEL 2 GOTO Install
+IF ERRORLEVEL 1 GOTO Fullsetup
+
+:Fullsetup
+Echo Installing dependancy
+pip install -r requirements.txt
+ECHO Setting up database
+flask db init
+ECHO Setup complete
+pause
+GOTO begin
+
+:Install
+Echo installing BOTICUS dependancy
+pip install -r requirements.txt
+pause
+GOTO begin
+
+:Builddb
+ECHO Setting up BOTICUS database
+flask db init
+pause
+GOTO begin
+
+:Run
+Echo Application will now start
+flask run
+
+:exit
+@exit
