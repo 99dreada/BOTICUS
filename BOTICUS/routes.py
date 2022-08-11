57c2d6115f9f4775a08cc58d27edc30045456fb4
99dreada <99dreada@live.co.uk>
2022-08-11 16:36:56 +0100

    update login

--- BOTICUS/routes.py
+++ BOTICUS/routes.py
@@ -1,6 +1,7 @@
 from flask import(
     render_template,
 )
+from flask_login import current_user, login_required
 from BOTICUS import app
 import BOTICUS.blueprints
 
@@ -9,6 +10,7 @@ import BOTICUS.blueprints
 ROUTING
 """
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
@@ -17,10 +19,12 @@ def about():
     return render_template('about.html')
 
 @app.route('/bots')
+@login_required
 def bots():
     return render_template('bots.html')
 
 @app.route('/content')
+@login_required
 def content():
     return render_template('content.html')
 
